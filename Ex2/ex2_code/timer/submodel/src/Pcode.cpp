a5c1338e22cb6010290ac713d0cdf2d567fa7ba9
Huib Donkers <h.t.donkers@student.tue.nl>
2015-07-01 18:07:31 +0200

    improved timer timing

--- Ex2/ex2_code/timer/submodel/src/Pcode.cpp
+++ Ex2/ex2_code/timer/submodel/src/Pcode.cpp
@@ -12,6 +12,10 @@
 // Each additional header should get a corresponding dependency in the Makefile
 #include <string.h>
 #include <sys/time.h>
+#include <sys/neutrino.h>
+#include <sys/syspage.h>
+#include <inttypes.h>
+#include <stdio.h>
 // protected region additional headers end
 
 namespace submodel { namespace Pcode { 
@@ -34,16 +38,22 @@ void Pcode::execute()
 {
   // protected region execute code on begin
 	static struct timespec t1;
-
+	static uint64_t clockcycle;
 
 	struct timespec t2;
+	struct timespec res;
+	uint64_t currentcycle = ClockCycles();
 
 	clock_gettime(CLOCK_REALTIME, &t2);
-	//long int elapsedTime = (t2.tv_usec - t1.tv_usec);
+	clock_getres(CLOCK_REALTIME, &res);
+	int64_t dcycle= currentcycle-clockcycle;
+	uint64_t cps = SYSPAGE_ENTRY(qtime)->cycles_per_sec;
 
-	printf("%ld.%.9ld, %ld.%.9ld\n", t2.tv_sec, t2.tv_nsec, t1.tv_sec, t1.tv_nsec);
+	if(currentcycle > clockcycle)
+		printf("%lld, %lld, %lld, %ld.%.9ld, %ld.%.9ld, %ld.%.9ld\n", currentcycle, clockcycle, cps, res.tv_sec, res.tv_nsec, t2.tv_sec, t2.tv_nsec, t1.tv_sec, t1.tv_nsec);
 
 	t1 = t2;
+	clockcycle = currentcycle;
 
 	// protected region execute code end
 }
